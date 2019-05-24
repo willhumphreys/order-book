8de47c6f0bdb2fa7fd6c7baa60f412f0c0017af5
Will Humphreys <whumphreys@gmail.com>
2019-05-24 09:50:10 +0900

    Initial commit

---
+++ src/main/java/codingtask/orderbook/OrderBookApplication.java
@@ -0,0 +1,13 @@
+package codingtask.orderbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderBookApplication.class, args);
+    }
+
+}
