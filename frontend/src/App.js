1d08958f1d4f18de844ec97a8e8c6a03065b095d
Will Humphreys <whumphreys@gmail.com>
2019-06-10 21:24:49 +0200

    Fix corrupted files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -15,161 +15,95 @@ class App extends Component {
     this.submitOrder = this.submitOrder.bind(this);
   }
 
-  submitOrder = () =;
-> {
-  const;
-  instrument = this.state.instrument;
+  submitOrder = () => {
+    const instrument = this.state.instrument;
 
-  fetch(
-
-  "/api/orderbooks/";
-+
-  instrument;
-+
-  "/orders";
-, {
-  method: "post";
-,
-  body: {
+    fetch("/api/orderbooks/" + instrument + "/orders", {
+      method: "post",
+      body: {
         quantity: 10,
         entryDate: "2019-06-09T09:04:29Z",
         price: 20
-  }
-}
+      }
+    })
+      .then(response => response.text())
+      .then(message => {
+        this.setState({ message: message });
+      });
+  };
 
-)
-.
-then(response = > response.text();
-)
-.
-then(message = > {
-  this.setState({message: message});
-})
-}
-submitOrder();
-{
+  submitOrder() {
     console.log("submit clicked");
-}
+  }
 
-render();
-{
+  render() {
     return (
-        < div >
-        < OrderForm / >
+      <div>
+        <OrderForm />
 
-        < Orders / >
+        <Orders />
 
-        < h2 > Order;
-  Statistics < /h2>
-  < OrderStatistic;
-  url = "/api/orderbooks/CSGN/orders/smallest" >
-      < b > Smallest;
-  Order < /b>
-  < br / >
-  < /OrderStatistic>
-  < OrderStatistic;
-  url = "/api/orderbooks/CSGN/orders/largest" >
-      < b > Largest;
-  Order < /b>
-  < br / >
-  < /OrderStatistic>
-  < OrderStatistic;
-  url = "/api/orderbooks/CSGN/orders/earliest" >
-      < b > Earliest;
-  Order < /b>
-  < br / >
-  < /OrderStatistic>
-  < OrderStatistic;
-  url = "/api/orderbooks/CSGN/orders/last" >
-      < b > Most;
-  Recent;
-  Order < /b>
-  < br / >
-  < /OrderStatistic>
-  < /div>;;
-)
-}
+        <h2>Order Statistics</h2>
+        <OrderStatistic url="/api/orderbooks/CSGN/orders/smallest">
+          <b>Smallest Order</b>
+          <br />
+        </OrderStatistic>
+        <OrderStatistic url="/api/orderbooks/CSGN/orders/largest">
+          <b>Largest Order</b>
+          <br />
+        </OrderStatistic>
+        <OrderStatistic url="/api/orderbooks/CSGN/orders/earliest">
+          <b>Earliest Order</b>
+          <br />
+        </OrderStatistic>
+        <OrderStatistic url="/api/orderbooks/CSGN/orders/last">
+          <b>Most Recent Order</b>
+          <br />
+        </OrderStatistic>
+      </div>
+    );
+  }
 }
 
-const OrderRenderer = ({order, children}) =;
->
-{
+const OrderRenderer = ({ order, children }) => {
   return (
-      < div >
-      {children};
-  {
-    order.quantity
-  }
-  {
-    order.price
-  }
-  Entry;
-  {
-    order.entryDate
-  }
-<
-  /div>;;
-)
-}
+    <div>
+      {children}
+      Quantity: {order.quantity}
+      Price: {order.price}
+      Entry Date: {order.entryDate}
+    </div>
+  );
+};
 
 class Orders extends React.Component {
   componentDidMount() {
     setInterval(this.requestOrders, 250);
   }
 
-  requestOrders = () =;
-> {
-  fetch(
+  requestOrders = () => {
+    fetch("/api/orderbooks/CSGN/orders")
+      .then(response => response.json())
+      .then(orders => {
+        this.setState({ orders });
+      });
+  };
 
-  "/api/orderbooks/CSGN/orders";
-)
-.
-
-  then(response =
-
->
-  response;
-.
-
-  json()
-
-)
-.
-
-  then(orders =
-
-> {
-  this;
-.
-
-  setState({orders});
-}
-
-)
-}
-constructor(props);
-{
+  constructor(props) {
     super(props);
-  this.state = {orders: []};
-}
-
-render();
-{
-    return (
-        < div >
-        < h2 > Orders < /h2>;;
-  {
-    this.state.orders.map((order, key) = > (
-    < OrderRenderer;
-    key = {order.id};
-    order = {order};
-    />;;
-  ))
+    this.state = { orders: [] };
   }
-<
-  /div>;;
-)
-}
+
+  render() {
+    return (
+      <div>
+        <h2>Orders</h2>
+        {this.state.orders.map((order, key) => (
+          <OrderRenderer key={order.id} order={order} />
+        ))}
+      </div>
+    );
+  }
 }
 
 class OrderStatistic extends React.Component {
@@ -180,59 +114,34 @@ class OrderStatistic extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {order: {}, url: props.url, title: props.children};
+    this.state = { order: {}, url: props.url, title: props.children };
   }
 
-  requestOrder = () =;
-> {
-  fetch(
+  requestOrder = () => {
+    fetch(this.state.url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else if (response.status === 404) {
+          return {};
+        }
+      })
+      .then(order => {
+        this.setState({ order });
+      });
+  };
 
-  this;
-.
-  state;
-.
-  url;
-)
-.
-
-  then(response =
-
-> {
-  if(response
-
-.
-  ok;
-) {
-  return;
-  response;
-.
-
-  json();
-}
-
-else
-if (response.status === 404) {
-  return {};
-}
-})
-.
-then(order = > {
-  this.setState({order});
-})
-}
-render();
-{
+  render() {
     return (
-        < OrderRenderer;
-  order = {this.state.order} > {this.state.title} < /OrderRenderer>;;
-)
-}
+      <OrderRenderer order={this.state.order}>{this.state.title}</OrderRenderer>
+    );
+  }
 }
 
 class OrderForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {instrument: "CSGN", quantity: "", price: "", entryDate: ""};
+    this.state = { instrument: "CSGN", quantity: "", price: "", entryDate: "" };
 
     this.handleQuantityChange = this.handleQuantityChange.bind(this);
     this.handlePriceChange = this.handlePriceChange.bind(this);
@@ -240,11 +149,11 @@ class OrderForm extends React.Component {
   }
 
   handleQuantityChange(event) {
-    this.setState({quantity: event.target.value});
+    this.setState({ quantity: event.target.value });
   }
 
   handlePriceChange(event) {
-    this.setState({price: event.target.value});
+    this.setState({ price: event.target.value });
   }
 
   handleSubmit(event) {
@@ -252,84 +161,50 @@ class OrderForm extends React.Component {
     event.preventDefault();
   }
 
-  submitOrder = () =;
-> {
-  const;
-  instrument = this.state.instrument;
+  submitOrder = () => {
+    const instrument = this.state.instrument;
 
-  fetch(
-
-  "/api/orderbooks/";
-+
-  instrument;
-+
-  "/orders";
-, {
-  method: "post";
-,
-  headers: {
+    fetch("/api/orderbooks/" + instrument + "/orders", {
+      method: "post",
+      headers: {
         Accept: "application/json",
         "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        quantity: this.state.quantity,
+        entryDate: new Date(),
+        price: this.state.price
+      })
+    })
+      .then(response => response.text())
+      .then(message => {
+        this.setState({ message: message });
+      });
   };
-,
-  body: JSON.stringify;
-( {
-  quantity: this;
-.
-  state;
-.
-  quantity;
-,
-  entryDate: new Date;
-(),
-  price: this;
-.
-  state;
-.
-  price
-}
 
-)
-})
-.
-then(response = > response.text();
-)
-.
-then(message = > {
-  this.setState({message: message});
-})
-}
-render();
-{
+  render() {
     return (
-        < form;
-  onSubmit = {this.handleSubmit} >
-      < label >
-      Quantity;
-:
-<
-  input;
-  type = "text";
-  value = {this.state.quantity};
-  onChange = {this.handleQuantityChange};
-  />
-  < /label>
-  < label >
-  Price;
-:
-<
-  input;
-  type = "text";
-  value = {this.state.price};
-  onChange = {this.handlePriceChange};
-  />
-  < /label>
-  < input;
-  type = "submit";
-  value = "Submit" / >
-      < /form>;;
-)
-}
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Quantity:
+          <input
+            type="text"
+            value={this.state.quantity}
+            onChange={this.handleQuantityChange}
+          />
+        </label>
+        <label>
+          Price:
+          <input
+            type="text"
+            value={this.state.price}
+            onChange={this.handlePriceChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 export default App;
